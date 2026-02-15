bf623dd8673346b5aa0fc5ad42f211763594e91f
snehathakur149 <75410708+snehathakur149@users.noreply.github.com>
2026-02-16 04:28:44 +0530

    Fixed  DB

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root75',
-    database: 'eco_store'
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
