a6b5ad48ba2ec939d3e24cc616f24785007aa0d1
snehathakur149 <75410708+snehathakur149@users.noreply.github.com>
2026-02-16 04:35:15 +0530

    Fixed DB variable name

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log("Database connection failed");
-    } else {
-        console.log("Connected to MySQL");
-    }
+  if (err) {
+    console.error("❌ Database connection failed:", err);
+  } else {
+    console.log("✅ Database Connected Successfully");
+  }
 });
 
 module.exports = connection;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
+const connection = require('../config/db');
 
 
 // ================= WELCOME =================
